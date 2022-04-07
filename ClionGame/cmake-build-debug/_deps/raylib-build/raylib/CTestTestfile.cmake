8ec666faa7b06ac119a7825c1c6d9e168f87baa4
Kendall Martinez <kendallmartinezcarvajal4@gmail.com>
2022-04-07 14:05:10 -0600

    prueba sin debug

--- ClionGame/cmake-build-debug/_deps/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/kenda/Desktop/TEC/Lenguajes/Lenguajes/Poll_Position/Tarea3_Pole_Position/ClionGame/cmake-build-debug/_deps/raylib-src/src
-# Build directory: C:/Users/kenda/Desktop/TEC/Lenguajes/Lenguajes/Poll_Position/Tarea3_Pole_Position/ClionGame/cmake-build-debug/_deps/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
