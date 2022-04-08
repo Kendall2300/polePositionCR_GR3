1ced976c59531e2dc3f2cc111fcbaf4dc73d69ce
Kendall Martinez <kendallmartinezcarvajal4@gmail.com>
2022-04-08 15:24:45 -0600

    Fuera debug cmake

--- ClionGame/cmake-build-debug/_deps/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/gaboc/OneDrive - Estudiantes ITCR/Documentos/GitHub/Tarea3_Poll_Position/ClionGame/cmake-build-debug/_deps/raylib-src/src
-# Build directory: C:/Users/gaboc/OneDrive - Estudiantes ITCR/Documentos/GitHub/Tarea3_Poll_Position/ClionGame/cmake-build-debug/_deps/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
